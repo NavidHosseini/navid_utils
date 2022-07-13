dd38f87110383dffc283cb137a2836901a0a1139
navidhosseini <navidhosseini1373@gmail.com>
2022-07-13 12:19:24 +0430

    update

--- lib/utils/shamsiDate/shamsiDate.dart
+++ lib/utils/shamsiDate/shamsiDate.dart
@@ -1,6 +1,6 @@
 import 'package:shamsi_date/shamsi_date.dart';
 
-Jalali nDateToJalali({required String date}) {
+Jalali nhDateToJalali({required String date}) {
   DateTime dateTime = DateTime.parse(date);
   return dateTime.toJalali();
 }
